c508114ba6f37b95888e4c9a6ea6a53304343311
You Matsumoto <youmts22@gmail.com>
2019-03-31 09:48:07 +0900

    exawizards2019/c

--- exawizards2019/c.rb
+++ exawizards2019/c.rb
@@ -4,36 +4,27 @@ def cout(*x); puts x.join(" ") end
 # exit if $0 != __FILE__
 
 n, q = cin
-s = gets.chop
 
-rs = []
-ls = []
+ss = gets.chop.chars
+ss.unshift("0")
+ss.push("0")
+ssi = [*(0..(ss.length-1))]
+
+qs = []
 q.times do
-  t,d=gets.split
-  rs << t if d == "R"
-  ls << t if d == "L"
+  t,d = gets.split
+  qs << [t,d]
 end
 
-def ff(ss, qq)
-  #p "ff"
-  #p ss
-  #p qq
-  cnt = 0
-  i = 0
-  ss.each do |c|
-    while i < qq.length
-      if qq[i] == c then
-        cnt += 1
-        i += 1
-        break
-      end
-      i += 1
-    end
+def exists?(ss, qs, i, e)
+  qs.each do |q|
+    i += (q[1] == "L") ? -1 : 1 if q[0] == ss[i]
+    return false if i == e
   end
-  cnt
+  true
 end
+    
+l = ssi.bsearch_index{|i|exists?(ss, qs, i, 0)} - 1
+r = ssi.bsearch_index{|i|!exists?(ss, qs, i, ss.length-1)}
 
-rr = ff(s.chars.reverse, rs.reverse)
-ll = ff(s.chars, ls.reverse)
-puts s.length - [rr+ll,s.length].min
-  
+puts [r-l-1,0].max
