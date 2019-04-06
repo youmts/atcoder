5024ccb03250e735f3ef947b74fe4980c8808722
You Matsumoto <youmts22@gmail.com>
2019-04-06 23:10:09 +0900

    abc123/abc,d(tle)

---
+++ abc123/a.rb
@@ -0,0 +1,16 @@
+def cin; gets.split.map(&:to_i) end
+def cout(*x); puts x.join(" ") end
+
+# exit if $0 != __FILE__
+
+a = gets.to_i
+b = gets.to_i
+c = gets.to_i
+d = gets.to_i
+e = gets.to_i
+k = gets.to_i
+if e-a <= k then
+  puts 'Yay!'
+  exit
+end
+puts ':('
---
+++ abc123/c.rb
@@ -0,0 +1,9 @@
+def cin; gets.split.map(&:to_i) end
+def cout(*x); puts x.join(" ") end
+
+# exit if $0 != __FILE__
+
+n = gets.to_i
+a = Array.new(5) {gets.to_i}
+m = a.min
+puts (n+m-1)/m+4
