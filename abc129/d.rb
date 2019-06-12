6004f813c1f880bf7926f123ae7e0d0e7653b411
You Matsumoto <youmts22@gmail.com>
2019-06-12 21:38:14 +0900

    abc128,abc129

---
+++ abc129/d.rb
@@ -0,0 +1,42 @@
+def cin; gets.split.map(&:to_i) end
+
+# exit if $0 != __FILE__
+
+# n, *x = cin
+# s = gets.chomp
+
+# puts n
+h,w = cin
+
+map = Array.new(h) {gets.chop.chars}
+
+yoko = []
+h.times do |y|
+  s = -1
+  a = []
+  w.times do |x|
+    c = map[y][x]
+    if c == "."
+      s = x if s < 0
+    else #
+      if s >= 0
+        a << [s, x-s]
+        s = -1
+      end
+    end
+  end
+  if s >= 0
+    a << [s, w-s]
+  end
+  yoko << a
+end
+
+def pa(a)
+  a.each do |x|
+    p x
+  end
+end
+
+pa yoko
+
+
---
+++ abc129/e.rb
@@ -0,0 +1,9 @@
+def cin; gets.split.map(&:to_i) end
+
+# exit if $0 != __FILE__
+
+# n, *x = cin
+# s = gets.chomp
+
+# puts n
+
